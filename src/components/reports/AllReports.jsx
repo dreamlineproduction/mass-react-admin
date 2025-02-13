e282d8eb0b6d23b2ee771f8800a575dbaa11f163
Ayan Mukhopadhyay <mahar@kresscooper.com>
2025-02-13 15:57:13 +0530

    report products

--- src/components/reports/AllReports.jsx
+++ src/components/reports/AllReports.jsx
@@ -7,7 +7,7 @@ const AllReports = () => {
 
 return (
 <div>
-    <PageTitle title="All Reports" />
+    <PageTitle title="Product Reports" />
     <div className="row">
         <div className="col-md-12">
             <div className="card">
@@ -17,33 +17,43 @@ return (
                             <thead>
                                 <tr>
                                     <th scope="col">ID</th>
-                                    <th scope="col">Report Title</th>
-                                    <th scope="col">Report Category</th>
-                                    <th scope="col">User</th>
-                                    <th scope="col">Date and Time</th>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">User Information</th>
+                                    <th scope="col">Reason</th>
+                                    <th scope="col">Reported Date</th>
+                                    <th scope="col">View Details</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">12</th>
-                                    <td>This product is not valid</td>
-                                    <td>Invalid</td>
-                                    <td>Sudip Dutta</td>
-                                    <td>12/01/2025 at 12:30PM</td>
+                                    <td><a href="#">Mass Wood Polish</a></td>
+                                    <td><a href="#">919836952666 (Ayan Mukhopadhyay)</a></td>
+                                    <td>Fake Product</td>
+                                    <td>12/01/2025</td>
+                                    <td>
+                                    <a title="View Top 50" className="btn btn-primary" href="/users/all-users">View Details</a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">12</th>
-                                    <td>This product is not valid</td>
-                                    <td>Invalid</td>
-                                    <td>Sudip Dutta</td>
-                                    <td>12/01/2025 at 12:30PM</td>
+                                    <td><a href="#">Mass Wood Polish</a></td>
+                                    <td><a href="#">919836952666 (Ayan Mukhopadhyay)</a></td>
+                                    <td>Fake Product</td>
+                                    <td>12/01/2025</td>
+                                    <td>
+                                    <a title="View Top 50" className="btn btn-primary" href="/users/all-users">View Details</a>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">12</th>
-                                    <td>This product is not valid</td>
-                                    <td>Invalid</td>
-                                    <td>Sudip Dutta</td>
-                                    <td>12/01/2025 at 12:30PM</td>
+                                    <td><a href="#">Mass Wood Polish</a></td>
+                                    <td><a href="#">919836952666 (Ayan Mukhopadhyay)</a></td>
+                                    <td>Fake Product</td>
+                                    <td>12/01/2025</td>
+                                    <td>
+                                    <a title="View Top 50" className="btn btn-primary" href="/users/all-users">View Details</a>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
